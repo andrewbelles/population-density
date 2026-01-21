c7d3422597353da0cf2a35a13c71db7a93a7adc0
andrewbelles <andrewbelles31@gmail.com>
2026-01-20 22:02:00 -0500

    flushing small changes

--- testbench/utils/config.py
+++ testbench/utils/config.py
@@ -66,5 +66,5 @@ def normalize_spatial_params(params, *, random_state: int, collate_fn):
     params.setdefault("early_stopping_rounds", 15)
     params.setdefault("eval_fraction", 0.15)
     params.setdefault("min_delta, 1e-3")
-    params.setdefault("batch_size", 4)
+    params.setdefault("batch_size", 8)
     return params 
